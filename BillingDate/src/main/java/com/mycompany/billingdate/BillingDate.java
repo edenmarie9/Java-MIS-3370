de4f45e7688cfe1cfc6f79ab4ca04a2c0e371514
edenmarie9 <80850857+edenmarie9@users.noreply.github.com>
2025-01-24 08:00:10 -0600

    Add files via upload

---
+++ BillingDate/src/main/java/com/mycompany/billingdate/BillingDate.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.billingdate;
+
+/**
+ *
+ * @author edenfreiku
+ */
+public class BillingDate {
+
+    public static void main(String[] args) {
+        int billingDate1 = 5;
+        int billingDate2;
+        billingDate2 = 15;
+        System.out.print("Bills are sent on day " + 
+                billingDate1 + "\nand on day " + billingDate2 +
+                        " of each month" );
+    }
+}
