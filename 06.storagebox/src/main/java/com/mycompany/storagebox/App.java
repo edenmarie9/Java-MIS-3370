cc947638d9bf894458416a9f03929c956b3fba1c
edenmarie9 <80850857+edenmarie9@users.noreply.github.com>
2025-01-24 08:02:59 -0600

    Add files via upload

---
+++ 06.storagebox/src/main/java/com/mycompany/storagebox/App.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.storagebox;
+
+/**
+ *
+ * @author edenfreiku
+ */
+public class App {
+
+    public static void main(String[] args) 
+    {
+        int livingRoomBox = 15;
+        int masterBedroomBox = 45;
+        int Bathroom = 10;
+        int total = (livingRoomBox + masterBedroomBox + Bathroom);
+        System.out.println(livingRoomBox);
+        System.out.println(masterBedroomBox);
+        System.out.println(Bathroom);
+        System.out.println(total);
+    }
+}
