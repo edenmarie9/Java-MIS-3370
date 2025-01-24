9b75efbba0a8d60dc8af64dd36b91a26181171a1
edenmarie9 <80850857+edenmarie9@users.noreply.github.com>
2025-01-24 08:04:29 -0600

    Add files via upload

---
+++ Ch2DebugProgram1/src/main/java/com/mycompany/ch2debugprogram1/Ch2DebugProgram1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ch2debugprogram1;
+
+/**
+ *
+ * @author edenfreiku
+ */
+public class Ch2DebugProgram1 {
+
+    public static void main(String[] args) {
+        int oneInt = 315;
+        double oneDouble = 12.4;
+        char oneChar = 'A';
+        System.out.print("The int is ");
+        System.out.println(oneInt);
+        System.out.print("The double is ");
+        System.out.println("oneDouble");
+        System.out.print("The char is ");
+        System.out.println(oneChar);
+        
+    }
+}
---
+++ Ch2DebugProgram2/src/main/java/com/mycompany/ch2debugprogram2/Ch2DebugProgram2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ch2debugprogram2;
+
+/**
+ *
+ * @author edenfreiku
+ */
+public class Ch2DebugProgram2 {
+
+    public static void main(String[] args) {
+        int a,b;
+        a = 7;
+        b = 4;
+        System.out.println("The sum is " + (a + b));
+        System.out.println("The difference is " + (a - b));
+        System.out.println("The product is " + (a - b));
+    }
+}
