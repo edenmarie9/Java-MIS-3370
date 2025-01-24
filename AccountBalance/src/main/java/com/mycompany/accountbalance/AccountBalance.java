18840987c24f9a16760acf473f605baf50bcd080
edenmarie9 <80850857+edenmarie9@users.noreply.github.com>
2025-01-24 08:02:19 -0600

    Add files via upload

---
+++ AccountBalance/src/main/java/com/mycompany/accountbalance/AccountBalance.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.accountbalance;
+
+/**
+ *
+ * @author edenfreiku
+ */
+public class AccountBalance {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
